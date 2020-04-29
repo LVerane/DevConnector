aeb354a8ffa6d355331ea6e5afeed6634cc627b5
Lucio Verane <verane64@gmail.com>
2020-04-29 11:45:55 -0500

    load user/ login/ logout added, navbar links change with logged in user

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,7 +3,17 @@ import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   const id = uuidv4();
-  dispatch({ type: SET_ALERT, payload: { msg, alertType, id } });
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id,
+      }),
+    timeout
+  );
 };
